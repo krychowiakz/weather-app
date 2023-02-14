b25c5791972e8b4cf275d5c1d6b4fb38c6e7cb8a
krychowiakz <124140276+krychowiakz@users.noreply.github.com>
2023-02-14 18:56:58 +0100

    Changed link and repaired current button

--- javascript.js
+++ javascript.js
@@ -23,9 +23,78 @@ function formatDate(date) {
     "Saturday",
   ];
   let day = days[dayIndex];
-
   return `${day} ${hours}:${minutes}`;
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function formatDay(time) {
+  let date = new Date(time * 1000);
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return day;
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let forecast = response.data.daily;
+  let forecastHTML = "";
+
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
+  <div class="col-2">
+    <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
+    
+    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+      forecastDay.condition.icon 
+    }.png" alt=""  />
+      <div class="weather-forecast-temperatures">
+     <span class="weather-forecast-temp-max"> ${Math.round(
+       forecastDay.temperature.maximum
+     )}°</span>
+     <span class="weather-forecast-temp-min"> ${Math.round(
+       forecastDay.temperature.minimum
+     )}°</span>
+     </div>
+  </div>
+`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function displayWeatherCondition(response) {
   let humidityElement = document.querySelector("#humidity");
@@ -69,17 +138,14 @@ function handleSubmit(event) {
   searchCity(city);
 }
 
-function searchLocation ( position) {
+function searchLocation(position) {
   let apiKey = "30e3tb3d590d3bcbf64oa3a03af0b501";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lat=${position.coords.latitude}&lon=${position.coords.longitude}&key=${apiKey}&units=metric`;
+  
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
+
 
 
 let celsiusTemperature = null;
@@ -91,8 +157,7 @@ dateElement.innerHTML = formatDate(currentTime);
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
+
 
 
 
@@ -128,4 +193,13 @@ function displayWeatherCondition(response) {
   getForecast(response.data.coordinates);
 }
 
-searchCity("New York");
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+
+searchCity("Warsaw");
